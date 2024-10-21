1e2841f3e5fdd4011ae9983d2ef966166c2d915d
Aryo Skuyyyy <aryoprayoga36yahoo@gmail.com>
2024-10-21 18:54:06 +0700

    as

--- src/utils/ChatUtils.js
+++ src/utils/ChatUtils.js
@@ -3,7 +3,6 @@ import { Groq } from "groq-sdk";
 const GROQ_API = import.meta.env.VITE_API_KEY_CHAT;
 const groq = new Groq({
   apiKey: GROQ_API,
-  dangerouslyAllowBrowser: true,
 });
 
 export const getChat = async (content) => {
@@ -17,5 +16,6 @@ export const getChat = async (content) => {
     model: "llama3-8b-8192",
     max_tokens: 150,
   });
+  console.log(response);
   return response.choices[0].message.content;
 };
