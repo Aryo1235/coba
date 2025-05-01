fb5ffbf2b66d6023cf754a6ff730743714980511
Aryo Skuyyyy <114992377+Aryo1235@users.noreply.github.com>
2025-05-01 12:04:33 +0700

    update rate

--- src/components/MovieInfo.jsx
+++ src/components/MovieInfo.jsx
@@ -1,10 +1,12 @@
-const MovieInfo = ({ movie }) => {
+const MovieInfo = ({ movie, userRating }) => {
+  const finalRating = userRating
+    ? ((Number(movie.vote_average) + Number(userRating)) / 2).toFixed(1)
+    : movie.vote_average.toFixed(1);
   return (
     <>
       <p className="text-lg mb-4">{movie.overview}</p>
       <p className="text-lg mb-2">
-        <span className="font-semibold">Rating:</span>{" "}
-        {movie.vote_average.toFixed(1)}
+        <span className="font-semibold">Rating:</span> {finalRating}
       </p>
       <p className="text-lg">
         <span className="font-semibold">Release Date:</span>{" "}
